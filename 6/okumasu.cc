e244574c6b07fd6582fc91c9123c43154cf1f44b
Takuya Kuhara <takuya.kuhataku@gmail.com>
2021-04-13 17:41:55 +0900

    Update to O(NlogNlogC)

--- 6/okumasu.cc
+++ 6/okumasu.cc
@@ -8,10 +8,10 @@ using namespace std;
 int main(int argc, char* argv[])
 {
 
-  int N, K;
+  int N, K, C;
   random_device rnd;
   mt19937 mt(rnd());
-  uniform_int_distribution<> rand1000(0,999);
+  uniform_int_distribution<> rand1000(1,9);
   cin >> N;
   cin >> K;
   vector<int> a(N);
@@ -22,26 +22,55 @@ int main(int argc, char* argv[])
     a[i] = rand1000(mt);
     b[i] = rand1000(mt);
   }
-
-
-  // N^2
   for (int i = 0; i < N; i++) {
-    for (int j = 0; j < N; j++) {
-      ab[i*N+j] = a[i] * a[j];
-      cout << ab[i*N+j] << ", ";
+    cout << a[i] << ",";
+  }
+  cout << endl;
+  for (int i = 0; i < N; i++) {
+    cout << b[i] << ",";
+  }
+  cout << endl;
+  // // logN
+  // sort(a.begin(), a.end());
+  // logN
+  sort(b.begin(), b.end());
+
+  C = a[N-1] * b[N-1];
+
+  // for (int i = 0; i < N; i++) {
+  //   // 各aに対してbの中から要素を探し掛け算する == candidate
+  //   // candidateより小さい値がK個存在しない => bを大きくする
+  //   // candidateより小さい値がK個より多い => bを小さくする
+  //   int b_right = N;
+  //   int b_left = 0;
+  //   int b_middle = 0;
+  //   long long int candidate;
+  //   while ( (b_right - b_left) > 1 ) {
+  //     b_middle = (b_right + b_left)/2;
+  //     candidate = a[i] * b[b_middle];
+  //   }
+  // }
+  
+  long long int ans = 1000000000000;
+  long long int right = C;
+  int left = 0;
+  while ( (right - left) > 1 ) {
+    int middle = (right + left)/2;
+    int count = 0;
+    for (int i = 0; i < N; i++) {
+      int upper_num = upper_bound(b.begin(), b.end(), (middle/a[i])) - b.begin();
+      count += upper_num;
     }
+    if ( count >= K ) {
+      if ( ans > middle ) {
+        ans = middle;
+      }
+      right = middle;
+    } else {
+      left = middle;
+    }
+    cout << middle << "," << count << endl;
   }
-
-  cout << endl;
-  // N^2logN
-  sort(ab.begin(), ab.end());
-
-  for(auto entry: ab) {
-    cout << entry << ", ";
-  }
-  cout << endl;
-
-  cout << ab[K];
-
+  cout << ans  << endl;
   return 0;
 }
